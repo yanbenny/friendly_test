78faf86e842ddc80fcd85d18b069d3c22fe7c037
benny <a0912542800@gmail.com>
2018-04-07 19:36:18 +0800

    dcard

--- example.js
+++ example.js
@@ -1,7 +1,7 @@
 var webdriver = require('selenium-webdriver');
+var cheerio = require('cheerio');
 var By = webdriver.By;
 var until = webdriver.until;
-
 var driver = new webdriver.Builder()
 	.forBrowser('chrome')
 	.build();
@@ -9,11 +9,11 @@ var driver = new webdriver.Builder()
 async function loadPage(url) {
 	
 	driver.get(url);
-	driver.sleep(10*1000).then(
+	driver.sleep(1*1000).then(
 		function(){
 		driver.getPageSource().then(
 			result => {
-				console.log(result);
+				resolvePageSource(result);
 			}
 		); 
 	});
@@ -23,7 +23,16 @@ async function loadPage(url) {
     },60*1000);
 }
 const resolvePageSource = (pagesource) =>{
-    console.log(pagesource);
+	//console.log(pagesource);
+	var $ = cheerio.load(pagesource);
+	var element=$('.PostList_entry_1rq5L .PostEntry_content_g2afg');
+	var items = [];
+	element.each(function (idx, element) {
+		const title = $(element).find('h3').text();
+		const heart = $(element).find('.Like_counter_1enlP').text();
+		console.log(title);
+		console.log(heart);
+      });
 }
 
 
