2975d7ba7925f4c5d2a2ab293891debe41d8e13a
benny <a0912542800@gmail.com>
2018-04-12 12:30:52 +0800

    change scroll

--- example.js
+++ example.js
@@ -9,17 +9,40 @@ var driver = new webdriver.Builder()
 async function loadPage(url) {
 	
 	driver.get(url);
-	driver.sleep(1*1000).then(
+	/*driver.sleep(1*1000).then(
 		function(){
 		driver.getPageSource().then(
 			result => {
 				resolvePageSource(result);
 			}
 		); 
-	});
+	});*/
+	await driver.getPageSource(); 
+    await driver.getPageSource().then(
+        result => {
+            resolvePageSource(result);
+	 });
+	 driver.executeScript('window.intervalId=setInterval(function(){scrollBy(0,200)},500)');
+	for(let i=0;i<3;i++)
+	 {
+		 await delayTimeAsync();
+		 await driver.getPageSource().then(
+		 	result =>{
+				 resolvePageSource(result);
+			 } 
+		 );
+	 }
+	 await driver.executeScript('clearInterval(window.intervalId)');
 	setTimeout(()=>{
         driver.quit();
     },60*1000);
+}
+const delayTimeAsync = (n=10)=> {
+	return new Promise((resolve,reject) => {
+		setTimeout(() =>{
+			resolve('timeout');
+		},n*1000);
+	});
 }
 const resolvePageSource = (pagesource) =>{
 	//console.log(pagesource);
