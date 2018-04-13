8e9802219f830be6d08bc014103266ddc82ba527
benny <a0912542800@gmail.com>
2018-04-14 02:29:32 +0800

    add module & main

--- example.js
+++ example.js
@@ -17,27 +17,27 @@ async function loadPage(url) {
 			}
 		); 
 	});*/
-	await driver.getPageSource(); 
-    await driver.getPageSource().then(
-        result => {
-            resolvePageSource(result);
-	 });
-	 driver.executeScript('window.intervalId=setInterval(function(){scrollBy(0,200)},500)');
-	for(let i=0;i<3;i++)
+	 await driver.getPageSource(); 
+	 driver.executeScript('window.intervalId=setInterval(function(){scrollBy(0,200)},300)');
+	 
+	 for(let i=0;i<4;i++)
 	 {
 		 await delayTimeAsync();
-		 await driver.getPageSource().then(
-		 	result =>{
-				 resolvePageSource(result);
-			 } 
-		 );
+		 if(i==3)
+		 {
+			await driver.getPageSource().then(
+			result =>{
+				resolvePageSource(result);
+				}
+			);
+		 }
 	 }
 	 await driver.executeScript('clearInterval(window.intervalId)');
 	setTimeout(()=>{
         driver.quit();
-    },60*1000);
+    },30*1000);
 }
-const delayTimeAsync = (n=10)=> {
+const delayTimeAsync = (n=6)=> {
 	return new Promise((resolve,reject) => {
 		setTimeout(() =>{
 			resolve('timeout');
@@ -52,10 +52,12 @@ const resolvePageSource = (pagesource) =>{
 	element.each(function (idx, element) {
 		const title = $(element).find('h3').text();
 		const heart = $(element).find('.Like_counter_1enlP').text();
-		console.log(title);
-		console.log(heart);
-      });
+		items.push({
+			title: title,
+			heart: heart
+		  });
+	  });
+	  console.log(items);
+	  return items;
 }
-
-
-loadPage('https://www.dcard.tw/f');
+module.exports=loadPage;
---
+++ main.js
@@ -0,0 +1,4 @@
+var loadPage = require('./example');
+loadPage('https://www.dcard.tw/f/yzu',(item)=>{
+    console.log(item);
+});
