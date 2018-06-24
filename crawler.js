3d5a92cbea9bf5b4c1bb4114fac761ed5177053a
yanbenny <37117594+yanbenny@users.noreply.github.com>
2018-06-24 23:26:04 +0800

    Add files via upload

--- crawler.js
+++ crawler.js
@@ -1,5 +1,5 @@
-var webdriver = require('selenium-webdriver');
-var cheerio = require('cheerio');
+var webdriver = require('/home/hi/桌面/nodejs/friendly_test-master/node_modules/selenium-webdriver');
+var cheerio = require('/home/hi/桌面/nodejs/friendly_test-master/node_modules/cheerio');
 var By = webdriver.By;
 var until = webdriver.until;
 var driver = new webdriver.Builder()
@@ -15,6 +15,7 @@ async function loadPage(url,callback) {
 	 await delayTimeAsync().then(()=>{
 	 	driver.executeScript('clearInterval(window.intervalId)');
 	 	driver.getPageSource().then((result)=>{
+			//console.log(resolvePageSource(result));
 			callback(resolvePageSource(result));
 			driver.quit();
 	 	})
@@ -30,19 +31,43 @@ const delayTimeAsync = (n=20)=>{
 }
 const resolvePageSource = (pagesource) =>{
 	var $ = cheerio.load(pagesource);
-	var element=$('.PostList_entry_1rq5L .PostEntry_content_g2afg');
+	var element=$('.PostList_entry_1rq5L');
 	var items = [];
 	element.each(function (idx, element) {
 		if(items.length<100){
-			const title = $(element).find('h3').text();
-			const heart = $(element).find('.Like_counter_1enlP').text();
+			var sex;
+			const title = $(element).find('.PostEntry_content_g2afg h3').text();
+			const sex1=$(element).find('.PostAuthorHeader_avatar_1V21V div').hasClass('AnonymousAvatar_male_3mpl_');
+			if(sex1)
+			{
+				sex='M';
+			}
+			const sex2=$(element).find('.PostAuthorHeader_avatar_1V21V div').hasClass('AnonymousAvatar_female_swqLg');
+			if(sex2)
+			{
+				sex='F'
+			}
+			if(!sex1&&!sex2){
+				sex='N'
+			}
+			const category =$(element).find('.PostEntry_header_1iheI .PostEntry_forum_1m8nJ').text();
+			var author= $(element).find('.PostEntry_header_1iheI .PostAuthor_root_3vAJf').text();
+			const heart = $(element).find('.PostEntry_content_g2afg .Like_counter_1enlP').text();
+			if(sex==='N'){
+				author='卡稱';
+			}
+			//console.log(author+sex+title+heart+category);
 			items.push({
 				title: title,
+				sex: sex,
+				category:category,
+				author:author,
 				heart: heart
 		  	});
 		}
 	  });
-	  console.log(items.length);
+	  
 	  return items;
 }
+//loadPage('https://www.dcard.tw/f');
 module.exports=loadPage;
--- mysql.js
+++ mysql.js
@@ -1,31 +1,90 @@
-var mysql      = require('mysql');
+var mysql      = require('/home/hi/桌面/nodejs/friendly_test-master/node_modules/mysql');
 var connection = mysql.createConnection({
   host     : '140.138.77.98',
   user     : 'yzujava201609',
   password : 'm0654284vm,6java',
   database : '2017_JAVA'
 });
- 
+var Today=new Date();
 
-function updateSql(result){
-	connection.connect();
-	var q="INSERT INTO mao VALUES";
-	var a=[{title:'687',heart:'0'},{title:'767',heart:'123'},{title:'mao',heart:'55'}];
-	result.forEach(function(element,index){
-		q=q+'("'+element.title+'",'+element.heart+')';
-		if(index!=result.length-1)
-		{
-		q=q+',';
+
+function updateSql(input){
+	
+	var Q_test='select * from Dcard limit 1;';
+	connection.query(Q_test,function (err, result) {
+		if(err){
+			console.log('[SELECT ERROR] - ',err.message);
+			return;
 		}
-		else
-		q=q+';';
+		if(result.length!=0){
+			//no empty--->delete old
+			connection.query('DELETE FROM Dcard_old',function(err){
+				if(err){
+					return;
+				}
+				
+				//connection.end();
+			})
+			//copy new to old
+			connection.query('INSERT Dcard_old SELECT * FROM Dcard',function (err, result) {
+				if(err){
+				console.log('[SELECT ERROR] - ',err.message);
+				return;
+				}
+				//connection.end();
+			});
+			//delete new
+			connection.query('DELETE FROM Dcard',function(err){
+				if(err){
+					console.log('[SELECT ERROR] - ',err.message);
+					return;
+				}
+				//connection.end();
+			});
+			//insert new
+			var q="INSERT INTO Dcard VALUES";
+			input.forEach(function(element,index){
+			q=q+'('+index+',"'+element.title+'",'+element.heart+',"'+element.sex+'","'+element.author+'")';
+			if(index!=input.length-1)
+			{
+				q=q+',';
+			}
+			else
+				q=q+';';
+			});
+			connection.query(q,function(err){
+				if(err){
+					console.log('[SELECT ERROR] - ',err.message);
+					return;
+				}
+			console.log(Today.getFullYear()+ "年" + (Today.getMonth()+1) + "月" + Today.getDate() + "日"+ Today.getHours()+"時"+Today.getMinutes()+"分" + Today.getSeconds()+'秒');
+			console.log("successful");
+			connection.end();
+			});			
+		}
+		else{
+			var q="INSERT INTO Dcard VALUES";
+			input.forEach(function(element,index){
+			q=q+'('+index+',"'+element.title+'",'+element.heart+',"'+element.sex+'","'+element.author+'")';
+			if(index!=input.length-1)
+			{
+				q=q+',';
+			}
+			else
+				q=q+';';
+			});
+			connection.query(q,function(err){
+				if(err){
+					console.log('[SELECT ERROR] - ',err.message);
+					return;
+				}
+			
+			connection.end();
+			});		
+		}
+	
 	});
-	connection.query(q,function (err, result) {
-		    if(err){
-		      console.log('[SELECT ERROR] - ',err.message);
-		      return;
-		    }
-	});
-	connection.end();
 }
+//updateSql('123');
+
 module.exports=updateSql;
